60b620a5196f11cf1e1da5d5bdd0234387e3454e
kaur22 <kaurannie2222@gmail.com>
2021-12-13 20:51:07 -0500

    Added Menu and fragments

--- app/src/main/java/com/appforall/justintimefinance/MainActivity.java
+++ app/src/main/java/com/appforall/justintimefinance/MainActivity.java
@@ -1,14 +1,83 @@
 package com.appforall.justintimefinance;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout menuLayout;
+    public ActionBarDrawerToggle actionBarToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuLayout = findViewById(R.id.drawer_layout);
+        actionBarToggle = new ActionBarDrawerToggle(this, menuLayout, R.string.nav_open, R.string.nav_close);
+
+        menuLayout.addDrawerListener(actionBarToggle);
+        actionBarToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setNavigationDrawer();
+
+    }
+
+    private void setNavigationDrawer() {
+        NavigationView navigationView =  findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                int id = item.getItemId();
+                if (id == R.id.menu_my_account) {
+                    fragment = new MyAccount();
+                } else if (id == R.id.menu_e_transfer) {
+                    fragment = new ETransfer();
+                } else if (id == R.id.menu_fund_transfer) {
+                    fragment = new FundTransfer(); // checking for different fragments
+                } else if (id == R.id.menu_transactions) {
+                    fragment = new Transactions(); // checking for different fragments
+                } else if (id == R.id.menu_activate_card) {
+                    fragment = new ActivateCard(); // checking for different fragments
+                } else if (id == R.id.logout) {
+                    fragment = new Logout(); // checking for different fragments
+                }
+
+                if (fragment != null)
+                    startFragment(fragment);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void startFragment(Fragment fragment) {
+        if (fragment != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame, fragment); //for start of new activity
+            transaction.commit();
+            menuLayout.closeDrawers();
+        }
     }
 }
