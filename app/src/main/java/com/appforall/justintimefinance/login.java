f216ec6ac209e1598fc7339e844651ee41e332e8
Sundaram Sharma <ssharma9324@conestogac.on.ca>
2021-12-15 16:09:05 -0500

    sundaram 4:08 pm 15 dec 2021

--- app/src/main/java/com/appforall/justintimefinance/login.java
+++ app/src/main/java/com/appforall/justintimefinance/login.java
@@ -6,17 +6,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
-    Button signUpBtn;
+    EditText edtEmail, edtPassword;
+    JITF_DBhelper DB;
+    Button loginBtn, signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signUpBtn =findViewById(R.id.signUpButtonTransfer);
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
+        signUpBtn = findViewById(R.id.signUpButtonTransferLogin);
+        loginBtn = findViewById(R.id.loginButtonWorkingLogin);
+
+        edtEmail = findViewById(R.id.inputEmailLogin);
+        edtPassword = findViewById(R.id.inputPasswordLogin);
+
+        DB = new JITF_DBhelper(this);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                if(email.equals("") || password.equals("")){
+                    Toast.makeText(login.this, "Please fill all the entries", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean checkemailpass = DB.checkingEmailPassword(email, password);
+                    if(checkemailpass == true){
+                        Toast.makeText(login.this, "Login Successful, welcome", Toast.LENGTH_SHORT).show();
+
+                        Intent intnt = new Intent(getApplicationContext(), MainActivity.class); //move from this activity to mainactivity
+                        startActivity(intnt); //start the new activity
+                    }else{
+                        Toast.makeText(login.this, "Invalid Email and password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() { //redirect the login activity to signup activity
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(login.this,signUp.class); // moving from login class to signUp class
--- app/src/main/java/com/appforall/justintimefinance/signUp.java
+++ app/src/main/java/com/appforall/justintimefinance/signUp.java
@@ -6,20 +6,69 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class signUp extends AppCompatActivity {
 
-    Button loginBtn;
+    EditText edtAccountNumber, edtName, edtEmail, edtPassword;
+    JITF_DBhelper DB;
+    Button loginBtn, signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        loginBtn=findViewById(R.id.loginButtonTransfer);
+
+        edtAccountNumber=(EditText) findViewById(R.id.inputAccountNumberSignup);
+        edtName=(EditText)findViewById(R.id.inputNameSignup);
+        edtEmail=(EditText)findViewById(R.id.inputEmailSignup);
+        edtPassword=(EditText)findViewById(R.id.inputPasswordSignup);
+
+        signUpBtn=(Button)findViewById(R.id.signUpButtonWorkingSignup);
+        loginBtn=(Button)findViewById(R.id.loginButtonTransferSignup);
+
+        DB= new JITF_DBhelper(this);
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String accountNumber = edtAccountNumber.getText().toString();
+                String email = edtEmail.getText().toString();
+                String name = edtName.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                if(accountNumber.equals("") || email.equals("") || name.equals("") || password.equals("")){
+                    Toast.makeText(signUp.this, "Please fill all the entries", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean checkemail = DB.checkingEmail(email); //checking if email already exist
+                    if(checkemail==false){
+                        Boolean insrt = DB.insertingData(accountNumber,email, name, password); //inserting data into database
+                        if(insrt == true) {
+                            Toast.makeText(signUp.this, "User Registered!", Toast.LENGTH_SHORT).show();
+
+                            Intent intnt = new Intent(getApplicationContext(), MainActivity.class); //move from this activity to mainactivity
+                            startActivity(intnt); //start the new activity
+                        }else{
+                            Toast.makeText(signUp.this, "User failed to register", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(signUp.this, "Email already exist!. Please Login", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(signUp.this,login.class);
+                Intent intent = new Intent(signUp.this,login.class); //move from this activity to mainactivity
                 startActivity(intent);//start the new activity
             }
         });
