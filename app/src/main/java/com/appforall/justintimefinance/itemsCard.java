5744e7f3716fed92e8d4103e22db03c38f5e3cc3
Sundaram Sharma <ssharma9324@conestogac.on.ca>
2021-12-16 14:05:57 -0500

    2:05 pm 16/21/2021

---
+++ app/src/main/java/com/appforall/justintimefinance/itemsCard.java
@@ -0,0 +1,25 @@
+package com.appforall.justintimefinance;
+
+public class itemsCard {
+
+    private String accountNumberCard, bankTypeCard;
+
+
+
+
+    public String getAccountNumberCard() {
+        return accountNumberCard;
+    }
+
+    public void setAccountNumberCard(String accountNumberCard) {
+        this.accountNumberCard = accountNumberCard;
+    }
+
+    public String getBankTypeCard() {
+        return bankTypeCard;
+    }
+
+    public void setBankTypeCard(String bankTypeCard) {
+        this.bankTypeCard = bankTypeCard;
+    }
+}
